92a4d6d9ffe85f76179105ab6718d8a734061885
Joel <joelruiz2@gmail.com>
2024-05-15 00:07:15 -0500

    fixing for deploy to acos-platform

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        // cssInjectedByJsPlugin(),
         // visualizer()
     ],
     clearScreen: false,
@@ -18,7 +15,6 @@ export default defineConfig({
         rollupOptions: {
             input: "./game-client/index.jsx",
             output: {
-                // manualChunks: undefined,
                 entryFileNames: `client.bundle.js`
             },
         },
