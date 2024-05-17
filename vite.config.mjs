ab770edbee42fc3c9850112fbfddd66b007756e7
Joel <joelruiz2@gmail.com>
2024-05-16 23:27:00 -0500

    cleaning up package and vite configs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        // cssInjectedByJsPlugin(),
         // visualizer()
     ],
     clearScreen: false,
@@ -15,6 +18,7 @@ export default defineConfig({
         rollupOptions: {
             input: "./game-client/index.jsx",
             output: {
+                // manualChunks: undefined,
                 entryFileNames: `client.bundle.js`
             },
         },
